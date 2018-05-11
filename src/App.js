5339ad8377fbb6710cda77d382c5c166cd47d5be
MathieuBouvet <mathieubouvet42@gmail.com>
2018-05-11 10:49:33 +0200

    Use LetterInput component in App.js to show all alphabet letters

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
+import LetterInput from "./LetterInput";
+
+const letters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 
 class App extends Component {
   render() {
@@ -8,6 +11,13 @@ class App extends Component {
         <header className="App-header">
           <h1 className="App-title">Hangman Game</h1>
         </header>
+        <div className="hangman-body">
+          <div className="letter-input-container">
+            {letters.map( (letter,index) => (
+              <LetterInput letter={letter} key={index} />
+            ))}
+          </div>
+        </div>
       </div>
     );
   }
