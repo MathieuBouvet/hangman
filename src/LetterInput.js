59ef029b1d2cc9f89178907a6c86e4d72d08e899
MathieuBouvet <mathieubouvet42@gmail.com>
2018-05-11 10:49:17 +0200

    Add basic display of letter for LetterInput component

---
+++ src/LetterInput.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./LetterInput.css"
+
+const LetterInput = ({letter}) => (
+	<div className="letter-input">
+		{letter}
+	</div>
+)
+
+export default LetterInput;
